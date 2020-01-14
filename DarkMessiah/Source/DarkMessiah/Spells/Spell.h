be84c812f8fb5bfb99b37d92f839a2bf177d124f
louissiani <l.siani92@gmail.com>
2020-01-14 15:21:19 +0100

    add weapon

---
+++ DarkMessiah/Source/DarkMessiah/Spells/Spell.h
@@ -0,0 +1,33 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Spell.generated.h"
+
+UCLASS()
+class DARKMESSIAH_API ASpell : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ASpell();
+
+protected:
+	/** Sphere collision component */
+	UPROPERTY(EditAnywhere, Category = Projectile)
+		class USphereComponent* CollisionComp;
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	/** called when projectile hits something */
+	UFUNCTION()
+		void OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit);
+
+};
