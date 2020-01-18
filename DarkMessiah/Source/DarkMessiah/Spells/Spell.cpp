e0c885d30484f3d7e2b0ae5d4f4c1bcaad704a66
SynysterRev <michael.arn@live.fr>
2020-01-18 17:58:51 +0100

    Add increasing fireball and more explosionForce if fireBall is increase

--- DarkMessiah/Source/DarkMessiah/Spells/Spell.cpp
+++ DarkMessiah/Source/DarkMessiah/Spells/Spell.cpp
@@ -20,7 +20,12 @@ ASpell::ASpell()
 	ProjectileMovement->ProjectileGravityScale = 0.0f;
 	ProjectileMovement->InitialSpeed = 0.0f;
 	ProjectileMovement->MaxSpeed = MaxSpeed;
+	CurrentScale = GetActorScale3D();
+	CurrentLocation = GetActorLocation();
+	PowerMultiplicator = 1.0f;
+	CurrentPowerMultiplicator = PowerMultiplicator;
 	RootComponent = CollisionComp;
+	
 }
 
 // Called when the game starts or when spawned
@@ -48,8 +53,48 @@ void ASpell::Tick(float DeltaTime)
 
 void ASpell::LaunchSpell(FVector _direction)
 {
+	if (UWorld* world = GetWorld())
+	{
+		if (world->GetTimerManager().IsTimerActive(TimerPreparation))
+		{
+			world->GetTimerManager().ClearTimer(TimerPreparation);
+		}
+	}
+	Strength *= CurrentPowerMultiplicator;
 	GetProjectileMovement()->Velocity = _direction * Speed;
 	SetLifeSpan(2.0f);
 	GetSphereComponent()->SetCollisionProfileName(TEXT("Projectile"));
 	//CollisionComp->OnComponentHit.AddDynamic(this, &ASpell::OnHit);		// set up a notification for when this component hits something blocking
+}
+
+void ASpell::PrepareLaunch()
+{
+	if (UWorld* world = GetWorld())
+	{
+		world->GetTimerManager().SetTimer(TimerPreparation, this, &ASpell::IncreasePower, TimerIncrease, true);
+	}
+
+}
+
+void ASpell::IncreasePower()
+{
+	if (CurrentScale.X < MaxScale.X && CurrentScale.Y < MaxScale.Y && CurrentScale.Z < MaxScale.Z)
+	{
+		CurrentScale += AddingScale;
+		CurrentLocation += AddingScale * 4.0f;
+		SetActorScale3D(CurrentScale);
+		CurrentPowerMultiplicator = FMath::Clamp(PowerMultiplicator + CurrentScale.X / MaxScale.X, PowerMultiplicator, MaxPowerMultiplicator);
+		
+		SetActorRelativeLocation(CurrentLocation);
+	}
+	else
+	{
+		if (UWorld* world = GetWorld())
+		{
+			if (world->GetTimerManager().IsTimerActive(TimerPreparation))
+			{
+				world->GetTimerManager().ClearTimer(TimerPreparation);
+			}
+		}
+	}
 }
